b953a0bd8eb571bf2ddef20648c9b38512653473
tanapolza42 <firsttanapol@outlook.com>
2020-05-05 12:27:58 +0700

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,18 @@
+
+public abstract class Shape {
+    private String name;
+    public Shape(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public abstract double getArea();
+}
+
+public interface Resizable{
+    public void resize(double factor);
+    public void reset();
+}
